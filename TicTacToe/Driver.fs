5709cf7620de8e0e911503e89da5388148a75eb5
Keith Owens <kowens@pekininsurance.com>
2016-06-22 15:58:24 -0500

    Refactored code majorly.  Added 10 tests.

---
+++ TicTacToe/Driver.fs
@@ -0,0 +1,13 @@
+﻿module Driver
+
+open System
+open GamePlayer
+
+
+[<EntryPoint>]
+let main (args : string[]) = 
+    let (startingBoard: char[]) = [| ' '; ' '; ' '; ' '; ' '; ' '; ' '; ' '; ' ' |]
+    printfn "Tic-Tac-Toe"
+
+    playGame (startingBoard, 1, -1, -1)
+    0
