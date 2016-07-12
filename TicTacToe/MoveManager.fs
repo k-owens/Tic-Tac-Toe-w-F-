66f37bb1d939ceaf28a84143eccd5b9508d5fada
KO87132 <KO87132@LHOFITS2PKP>
2016-07-12 10:34:15 -0500

    Add feature to pick type of player and added Player type.

--- TicTacToe/MoveManager.fs
+++ TicTacToe/MoveManager.fs
@@ -6,6 +6,7 @@ open RuleAlgorithm3X3
 open RuleAlgorithm4X4
 open StupidComputer
 
+
 let rec humanMove (input,game, enterMove : unit -> string, print : string -> unit) =
     let move = input(enterMove,print)
     if isLegalMove (move, game) then
@@ -13,13 +14,20 @@ let rec humanMove (input,game, enterMove : unit -> string, print : string -> uni
     else
         humanMove (input,game,enterMove,print)
 
-let computerMove (gameState : Game, algorithmSeed) : int =
+let computerMove (gameState : Game, algorithmSeed,  print : string -> unit, askingPlayer, opposingPlayer) : int =
+    print ("Computer Move:\n")
     if algorithmSeed = 1  then
-        let result = minimaxAlgorithm (gameState,true)
+        let result = minimaxAlgorithm (gameState,true, askingPlayer, opposingPlayer)
         result.[0]
     elif(algorithmSeed = 3 && gameState.BoardSize = 3) then
-        rule3X3(gameState,0,0,gameState.HumanCharacter,gameState.ComputerCharacter)
+        rule3X3(gameState,0,0,opposingPlayer.PlayerCharacter,askingPlayer.PlayerCharacter)
     elif(algorithmSeed = 3 && gameState.BoardSize = 4) then
-        rule4X4(gameState, gameState.HumanCharacter, gameState.ComputerCharacter)
+        rule4X4(gameState,opposingPlayer.PlayerCharacter,askingPlayer.PlayerCharacter)
     else
         stupidComputerMove(gameState, gameState.BoardSize*gameState.BoardSize-1)
+        
+let moveTypeSelector (player : Player,input,game, enterMove : unit -> string, print : string -> unit,gameState : Game, algorithmSeed, opposingPlayer) =
+    if(player.PlayerSeed = 1) then
+        humanMove (input,game, enterMove, print)
+    else
+        computerMove (gameState, player.ComputerAlgorithm, print, player,opposingPlayer)
