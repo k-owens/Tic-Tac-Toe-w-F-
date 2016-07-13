6e3aed2c27b82286182164d4935f15ef9805f99f
KO87132 <KO87132@LHOFITS2PKP>
2016-07-13 08:39:07 -0500

    Redesign and cleanup for the MoveManager file.

--- TicTacToe/MoveManager.fs
+++ TicTacToe/MoveManager.fs
@@ -6,28 +6,20 @@ open RuleAlgorithm3X3
 open RuleAlgorithm4X4
 open StupidComputer
 
-
-let rec humanMove (input,game, enterMove : unit -> string, print : string -> unit) =
-    let move = input(enterMove,print)
-    if isLegalMove (move, game) then
+let rec humanMove (spaceInput,game : Game) =
+    let move = spaceInput(game.Functions.InputFunction,game.Functions.PrintFunction)
+    if isLegalMove (move, game.GameBoard) then
         move
     else
-        humanMove (input,game,enterMove,print)
+        humanMove (spaceInput,game)
 
-let computerMove (gameState : Game, algorithmSeed,  print : string -> unit, askingPlayer, opposingPlayer) : int =
-    print ("Computer Move:\n")
-    if algorithmSeed = 1  then
-        let result = minimaxAlgorithm (gameState,true, askingPlayer, opposingPlayer)
-        result.[0]
-    elif(algorithmSeed = 3 && gameState.BoardSize = 3) then
-        rule3X3(gameState,0,0,opposingPlayer.PlayerCharacter,askingPlayer.PlayerCharacter)
-    elif(algorithmSeed = 3 && gameState.BoardSize = 4) then
-        rule4X4(gameState,opposingPlayer.PlayerCharacter,askingPlayer.PlayerCharacter)
+
+let computerMove (game : Game, playerNumber) = 
+    game.Functions.PrintFunction ("Computer Move:\n")
+    game.Players.[playerNumber-1].ComputerAlgorithm (game.GameBoard, game.Players.[playerNumber-1], otherPlayer(game,playerNumber))
+
+let moveTypeSelector (game : Game, playerNumber, spaceInput) = 
+    if(game.Players.[playerNumber-1].PlayerType = 1) then
+        humanMove (spaceInput,game)
     else
-        stupidComputerMove(gameState, gameState.BoardSize*gameState.BoardSize-1)
-        
-let moveTypeSelector (player : Player,input,game, enterMove : unit -> string, print : string -> unit,gameState : Game, algorithmSeed, opposingPlayer) =
-    if(player.PlayerSeed = 1) then
-        humanMove (input,game, enterMove, print)
-    else
-        computerMove (gameState, player.ComputerAlgorithm, print, player,opposingPlayer)
+        computerMove (game, playerNumber)
