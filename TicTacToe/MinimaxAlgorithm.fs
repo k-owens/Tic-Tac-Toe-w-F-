9784c345220868f836a260c27e3fd21f7a08edae
KO87132 <KO87132@LHOFITS2PKP>
2016-07-18 07:08:56 -0500

    Take out the mutable variables in the minimax algorithm.

--- TicTacToe/MinimaxAlgorithm.fs
+++ TicTacToe/MinimaxAlgorithm.fs
@@ -2,40 +2,37 @@
 open GameBoard
 open Game
 
+let availableBoards (gameBoard : Board, player) =
+    let moves = [for location in 0 .. (gameBoard.BoardSize*gameBoard.BoardSize - 1) -> if(gameBoard.CurrentBoard.[location] = None) then Some(location) else None] |> List.choose id
+    [for location in 0 .. (List.length moves)-1 -> {BoardSize = gameBoard.BoardSize; CurrentBoard = makeMove(gameBoard,moves.[location],player.PlayerCharacter); TurnNumber = gameBoard.TurnNumber+1; IsInverted = gameBoard.IsInverted}]
 
-let rec minimaxAlgorithm (gameBoard : Board, isPlayer, askingPlayer : Player, opposingPlayer : Player) = 
+
+
+let rec minimaxAlgorithm (gameBoard : Board, isPlayer, askingPlayer : Player, opposingPlayer : Player) =              
     if(didPlayer1Win (gameBoard.CurrentBoard, askingPlayer.PlayerCharacter, gameBoard.BoardSize)) then
         (0,1)
     elif(didPlayer2Win (gameBoard.CurrentBoard, opposingPlayer.PlayerCharacter, gameBoard.BoardSize)) then
         (0,-1)
     elif(didTieHappen gameBoard.CurrentBoard) then
         (0,0)
+    elif isPlayer then
+        let moves = [for location in 0 .. (gameBoard.BoardSize*gameBoard.BoardSize - 1) -> if(gameBoard.CurrentBoard.[location] = None) then Some(location) else None] |> List.choose id
+        let futureBoards = availableBoards(gameBoard, askingPlayer)
+        let results = [for i in 0 .. (List.length futureBoards)-1 ->minimaxAlgorithm(futureBoards.[i], false, askingPlayer,opposingPlayer)]
+        let scores = [for i in 0 .. (List.length results)-1 -> snd results.[i]]
+        let maxScore = List.max scores
+        let location = List.findIndex(fun elem -> elem = maxScore) scores
+        (moves.[location],maxScore)
     else
-        if isPlayer then
-            let mutable maximum = -1000
-            let mutable resultArray = [||]
+        let moves = [for location in 0 .. (gameBoard.BoardSize*gameBoard.BoardSize - 1) -> if(gameBoard.CurrentBoard.[location] = None) then Some(location) else None] |> List.choose id
+        let futureBoards = availableBoards(gameBoard, opposingPlayer)
+        let results = [for i in 0 .. (List.length futureBoards)-1 ->minimaxAlgorithm(futureBoards.[i], true, askingPlayer,opposingPlayer)]
+        let scores = [for i in 0 .. (List.length results)-1 -> snd results.[i]]
+        let minScore = List.min scores
+        let location = List.findIndex(fun elem -> elem = minScore) scores
+        (moves.[location],minScore)
 
-            for index in 0 .. (gameBoard.BoardSize*gameBoard.BoardSize - 1) do
-                if(gameBoard.CurrentBoard.[index] = None) then
-                    let newGameBoard = {BoardSize = gameBoard.BoardSize; CurrentBoard = makeMove(gameBoard,index,askingPlayer.PlayerCharacter); TurnNumber = gameBoard.TurnNumber+1; IsInverted = gameBoard.IsInverted}
-                    let result = minimaxAlgorithm(newGameBoard, false, askingPlayer,opposingPlayer)
-                    if maximum < snd result then
-                        maximum <- snd result
-                        resultArray <- [|index;maximum|]
-            (resultArray.[0], resultArray.[1])
-        else
-            let mutable minimum = 1000
-            let mutable resultArray = [||]
-
-            for index in 0 .. (gameBoard.BoardSize*gameBoard.BoardSize - 1) do
-                if(gameBoard.CurrentBoard.[index] = None) then
-                    let newGameBoard = {BoardSize = gameBoard.BoardSize; CurrentBoard = makeMove(gameBoard,index,opposingPlayer.PlayerCharacter); TurnNumber = gameBoard.TurnNumber+1; IsInverted = gameBoard.IsInverted}
-                    let result = minimaxAlgorithm(newGameBoard, true, askingPlayer,opposingPlayer)
-                    if minimum > snd result then
-                        minimum <- snd result    
-                        resultArray <- [|index;minimum|]
-            (resultArray.[0], resultArray.[1])
             
-let minimaxStarter (gameBoard : Board, askingPlayer : Player, opposingPlayer : Player) =
-    let result = minimaxAlgorithm(gameBoard, true, askingPlayer, opposingPlayer)
+let minimaxStarter (gameBoard : Board, askingPlayer : Player, opposingPlayer : Player) : int =
+    let result = minimaxAlgorithm(gameBoard,true,askingPlayer,opposingPlayer)
     fst result
